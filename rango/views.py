e80bddebdb829fba48e087edc1a14af41ff42e7c
Iain Cattermole <2189570c@student/gla.ac.uk>
2017-01-27 10:49:57 +0000

    Up to Exercises chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key to boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+        category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 	
 	# Return a rendered response to send to the client.
 	# We make use of the shortcut function to make our lives easier.
@@ -16,3 +17,27 @@ def index(request):
 	
 def about(request):
         return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+        context_dict = {}
+
+        try:
+                # Find the Category with the given slug
+                category = Category.objets.get(slug=category_name_slug)
+
+                # Retrieve all associated pages
+                pages = Page.objects.filter(category=category)
+
+                # Adds pages list to template under name pages
+                context_dict['pages'] = pages
+
+                # Add the category in object form from the database
+                # Use this to verify that the category exists
+                context_dict['category'] = category
+
+        except Category.DoesNotExist:
+                # if category does not exist then do nothing
+                context_dict['category'] = None
+                context_dict['pages'] = None
+
+        return render(request, 'rango/category.html', context_dict)
