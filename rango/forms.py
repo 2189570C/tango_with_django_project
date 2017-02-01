89afabefd35474b44800268313fea75afa220fa4
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-02-01 14:56:39 +0000

    Chapter 7 exercises

--- rango/forms.py
+++ rango/forms.py
@@ -34,3 +34,15 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
         # fields = ('title', 'url', 'views')
+	
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		
+		# if url is not empty and doesn't start with 'http://',
+		# then prepend 'http://'.
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+			
+			return cleaned_data
