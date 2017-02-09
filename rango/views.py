bd7debf053a061f05964a1513f793627ae76128f
Iain Cattermole <2189570c@student/gla.ac.uk>
2017-02-09 19:46:23 +0000

    Finished Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -5,23 +5,54 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from datetime import datetime
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+        val = request.session.get(cookie)
+        if not val:
+                val = default_val
+        return val
+
+# Helper Function
+def visitor_cookie_handler(request):
+        visits = int(get_server_side_cookie(request, 'visits', '1'))
+        last_visit_cookie = get_server_side_cookie(request,
+                                                   'last_visit',
+                                                   str(datetime.now()))
+        last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                            '%Y-%m-%d %H:%M:%S')
+
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).days > 0:
+                visits = visits + 1
+                #update the last visit cookie now that we have updated the count
+                request.session['last_visit'] = str(datetime.now())
+        else:
+                # set the last visit cookie
+                request.session['last_visit'] = last_visit_cookie
+        # Update/set the visits cookie
+        request.session['visits'] = visits
+
+
 
 def index(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key to boldmessage is the same as {{ boldmessage }} in the template!
         category_list = Category.objects.order_by('-likes')[:5]
         pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': pages_list}
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 	
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
-	return render(request, 'rango/index.html', context=context_dict)
+        # Obtain our Response object early so we can add cookie information.
+        response = render(request, 'rango/index.html', context_dict)
+        return response
 
 
 	
 def about(request):
-        return render(request, 'rango/about.html')
+        context_dict = {'visits': request.session['visits'] }
+        return render(request, 'rango/about.html', context_dict)
 
 def show_category(request, category_name_slug):
         context_dict = {}
